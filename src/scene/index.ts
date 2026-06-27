c63cae375466e4cc5864450d1e2fab89f3b55cb9
kemolumi <31346263+kemolumi@users.noreply.github.com>
2026-06-27 21:00:26 +0700

    Forgor remove test component

--- src/scene/index.ts
+++ src/scene/index.ts
@@ -1,6 +1,5 @@
 import BakeScene from './components/BakeScene';
 import SceneSync from './components/SceneSync';
-import TestConsume from './components/TestConsume';
 import { useViewport } from './hooks/viewport';
 
-export { BakeScene, SceneSync, TestConsume, useViewport };
+export { BakeScene, SceneSync, useViewport };
